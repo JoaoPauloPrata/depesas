c9bb46c96c6d8dd630ba84c03b1b114befb8275e
Joao Paulo Prata Costa <joaoppcosta@hotmail.com.br>
2022-01-18 22:56:22 -0300

    Versão inicial da pagina de despesas

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,19 +1,37 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: avoid_unnecessary_containers
 
-main() => runApp(const ExpensesApp());
+import 'package:expenses/model/transaction.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({Key? key}) : super(key: key);
+  ExpensesApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+  final _transactions_ = [
+    Transaction(
+        id: 't1',
+        title: 'Novo Tênis da Nikeeee',
+        value: 254.56,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Conta de Luz da Cemig',
+        value: 150.2,
+        date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +39,6 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Despesas Pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
@@ -30,9 +47,72 @@ class MyHomePage extends StatelessWidget {
             color: Colors.blue,
             child: Text('Gráfico'),
           )),
+          Column(
+            children: _transactions_
+                .map((tr) => Card(
+                      child: Row(
+                        children: [
+                          Container(
+                            // ignore: prefer_const_constructors
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.black, width: 2)),
+                            padding: const EdgeInsets.all(10),
+                            child: Text(
+                              'R\$ ${tr.value.toStringAsFixed(2)}',
+                              style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.purple),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                tr.title,
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                DateFormat('d MMM y').format(tr.date),
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.grey),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ))
+                .toList(),
+          ),
           Card(
-            child: Text('Lista de Transações'),
-          )
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(labelText: 'Titulo'),
+                    ),
+                    const TextField(
+                        decoration: InputDecoration(
+                      labelText: 'Valor (R\$)',
+                    )),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          child: const Text('Nova Transação'),
+                          onPressed: () {},
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ))
         ],
       ),
     );
