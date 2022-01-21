8a1f71cf07c29cf80087c1e56c5ab28b492dc8d6
Joao Paulo Prata Costa <joaoppcosta@hotmail.com.br>
2022-01-20 22:33:58 -0300

    Passando formulario para um modal que abre somente ao clicar no botão 'adicionar despesa'

--- expenses/lib/components/transaction_form.dart
+++ expenses/lib/components/transaction_form.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
+class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
   TransactionForm(this.onSubmit);
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+    widget.onSubmit(title, value);
+  }
+
   final titleController = TextEditingController();
+
   final valueController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -15,25 +32,21 @@ class TransactionForm extends StatelessWidget {
             children: [
               TextField(
                 controller: titleController,
-                decoration: InputDecoration(labelText: 'Titulo'),
+                decoration: const InputDecoration(labelText: 'Titulo'),
               ),
               TextField(
+                  keyboardType: TextInputType.number,
                   controller: valueController,
-                  decoration: InputDecoration(
+                  onSubmitted: (_) => _submitForm(),
+                  decoration: const InputDecoration(
                     labelText: 'Valor (R\$)',
                   )),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    child: const Text('Nova Transação'),
-                    onPressed: () {
-                      final title = titleController.text;
-                      final value =
-                          double.tryParse(valueController.text) ?? 0.0;
-                      onSubmit(title, value);
-                    },
-                  ),
+                      child: const Text('Nova Transação'),
+                      onPressed: _submitForm),
                 ],
               )
             ],
--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,10 +1,14 @@
 // ignore_for_file: avoid_unnecessary_containers
 
-import 'package:expenses/components/transaction_user.dart';
+import 'dart:math';
 
+import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+import 'components/transaction_list.dart';
+import 'model/transaction.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -18,29 +22,73 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   MyHomePage();
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions_ = [
+    Transaction(
+        id: 't1',
+        title: 'Novo Tênis da Nikeeee',
+        value: 254.56,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Conta de Luz da Cemig',
+        value: 150.2,
+        date: DateTime.now()),
+  ];
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+        id: Random().nextDouble().toString(),
+        title: title,
+        value: value,
+        date: DateTime.now());
+    setState(() {
+      _transactions_.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(_addTransaction);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () => _openTransactionFormModal(context),
+              icon: Icon(Icons.add))
+        ],
         title: const Text('Despesas Pessoais'),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-                child: Card(
-              elevation: 5,
-              color: Colors.blue,
-              child: Text('Gráfico'),
-            )),
-            TransactionUser()
-          ],
-        ),
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Container(
+              child: Card(
+            elevation: 5,
+            color: Colors.blue,
+            child: Text('Gráfico'),
+          )),
+          TransactionList(_transactions_),
+        ]),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _openTransactionFormModal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
